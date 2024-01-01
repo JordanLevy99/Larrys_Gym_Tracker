3a4699349cfdc0eb6080a5670aa36e4c2ca7ef03
JordanLevy99 <jordanlevy99@gmail.com>
2023-12-31 18:57:35 -0800

    v1 of bot is released

--- src/bot.py
+++ src/bot.py
@@ -10,28 +10,47 @@ from pydrive.drive import GoogleDrive
 import pytz
 import pandas as pd
 import requests
+from discord import Permissions
+from datetime import timedelta
 
+# Constants
+db_file = 'larrys_database.db'
+text_channel = 'larrys-gym-planner-name-subject-to-change'
+current_text_channel = lambda member: discord.utils.get(member.guild.text_channels, name=text_channel)
+voice_channel = 'Larry\'s Gym'
+start_hour = 7
+end_hour =  9
+verbose = False
+length_of_walk_in_minutes = 45
+max_on_time_points = 50
+max_duration_points = 50
 
-if __name__ == '__main__':
-    # Load the .env file
-    load_dotenv()
+# Load the .env file
+load_dotenv()
 
-    # Get the bot token
-    bot_token = os.getenv('BOT_TOKEN')
+permissions = Permissions(0x00000400 | 0x00000800)
+# Get the bot token
+bot_token = os.getenv('BOT_TOKEN')
 
-    intents = discord.Intents.default()
-    intents.message_content = True
+intents = discord.Intents.default()
+intents.message_content = True
 
-    bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-    # Connect to the SQLite database
-    conn = sqlite3.connect('voice_log.db')
-    c = conn.cursor()
-    
-    # Create table if it doesn't exist
-    c.execute('''CREATE TABLE IF NOT EXISTS voice_log
-                (name text, id text, join_time text, channel text)''')
+# Connect to the SQLite database
+conn = sqlite3.connect(db_file)
+c = conn.cursor()
 
+# Create table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS voice_log
+            (name text, id text, time datetime, channel text, user_joined boolean)''')
+
+# Create table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS points
+            (name text, id text, points_awarded float, time datetime, type text)''')
+
+def download():
+    global conn, c, drive
     # Authenticate with Google Drive
     gauth = GoogleAuth()
     # gauth.LocalWebserverAuth()
@@ -40,93 +59,159 @@ if __name__ == '__main__':
     drive = GoogleDrive(gauth)
 
     # Check if the file exists in Google Drive
-    file_list = drive.ListFile({'q': "title='voice_log.csv'"}).GetList()
-    print(file_list)
+    file_list = drive.ListFile({'q': f"title='{db_file}'"}).GetList()
     if file_list:
         # Download the file
         file = file_list[0]
-        file.GetContentFile('voice_log.csv')
-        # print(file)
+        file.GetContentFile(db_file)
 
         # URL of the file in Google Drive
         file_id = file['id']
-
-        # Extract the file ID from the URL
 
         # Construct the download link
         download_link = f'https://drive.google.com/uc?id={file_id}'
         print(download_link)
         requests.get(download_link)
         # Read the file into a pandas DataFrame
-        df = pd.read_csv('voice_log.csv')
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+        df = pd.read_sql_query("SELECT * FROM voice_log", conn)
 
-        # Print the DataFrame
-        print(df)
-        # df = pd.read_csv(file)
-        df.to_sql('voice_log.db', conn, if_exists='append', index=False)
-        print('Downloaded voice_log.csv from Google Drive!')
+        # Print the last five entries of the DataFrame
+        print(df.tail())
+        df.to_sql(db_file, conn, if_exists='replace', index=False)
+        print(f'Downloaded {db_file} from Google Drive!')
     else:
-        print('voice_log.csv does not exist in Google Drive.')
+        print(f'{db_file} does not exist in Google Drive.')
+    # return conn, c, drive
 
-    bot.run(bot_token)
-
-    # Close the connection to the database
-    conn.close()
-
+download()
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hello, world!')
-
-def upload():
-    # Query the voice_log table
-    df = pd.read_sql_query("SELECT * FROM voice_log", conn)
-    
-    # Save the table as a CSV file
-    csv_file = 'voice_log.csv'
-    df.to_csv(csv_file, index=False)
-    
-    # Authenticate with Google Drive
-    gauth = GoogleAuth()
-    # gauth.LocalWebserverAuth()
-    
-    # Create a Google Drive instance
-    drive = GoogleDrive(gauth)
-    
+async def upload(channel):
+    # Query the voice_log table    
     # Check if the file already exists in Google Drive
-    file_list = drive.ListFile({'q': f"title='{csv_file}'"}).GetList()
+    file_list = drive.ListFile({'q': f"title='{db_file}'"}).GetList()
     if file_list:
         # Update the existing file
         file = file_list[0]
-        file.SetContentFile(csv_file)
+        file.SetContentFile(db_file)
         file.Upload()
-        print(f'Updated {csv_file} in Google Drive!')
+        print(f'Updated {db_file} in Google Drive!')
     else:
         # Upload the CSV file to Google Drive
-        file = drive.CreateFile({'title': csv_file})
-        file.SetContentFile(csv_file)
+        file = drive.CreateFile({'title': db_file})
+        file.SetContentFile(db_file)
         file.Upload()
-        print(f'Uploaded {csv_file} to Google Drive!')
+        print(f'Uploaded {db_file} to Google Drive!')
+
+async def add_points(text_channel):
+    print('Points query results:')
+    users_df = pd.read_sql_query("""
+                            SELECT name, id, time 
+                            FROM voice_log
+                            GROUP BY time, id
+                            ORDER BY time DESC 
+                            LIMIT 2""", conn)
+    users_df['time'] = users_df['time'].astype('datetime64[ns]')
+    users_durations = users_df.groupby('id').apply(lambda user: user['time'].max() - user['time'].min())
+    await calculate_points(text_channel, users_df, users_durations)
+    print(pd.read_sql_query("""SELECT * FROM points""", conn).tail())
+
+
+async def calculate_points(text_channel, users_df, users_durations):
+    walk_time_in_seconds = timedelta(minutes=length_of_walk_in_minutes).total_seconds()
+    duration_points = (users_durations.dt.total_seconds() / walk_time_in_seconds) * 50
+    duration_points.loc[duration_points>max_duration_points] = max_duration_points
+    late_time = (users_df.groupby('id').apply(lambda user: user['time'].min() - user['time'].min().replace(hour=start_hour, minute=0, second=0)))
+    on_time_points = max_on_time_points - (late_time.dt.total_seconds() / (walk_time_in_seconds / 2)) * 50
+    on_time_points.loc[on_time_points<0] = 0
+
+    await process_points_df(text_channel, users_df, on_time_points, 'ON TIME')
+    await process_points_df(text_channel, users_df, duration_points, 'DURATION')    
+
+async def process_points_df(text_channel, users_df, points_df, points_type):
+    points_df.name = 'points_awarded'
+    points_df = points_df.to_frame()
+    points_df['type'] = points_type
+    points_df = points_df.merge(users_df.loc[users_df['time'] == users_df['time'].max(), ['name', 'id', 'time']], how='left', left_on='id', right_on='id')
+    points_df = points_df[['name', 'id', 'points_awarded', 'time', 'type']]
+    if len(points_df) == 1:
+        await text_channel.send(f"Awarded {points_df['points_awarded'].values[0]} points to {points_df['name'].values[0]} for {points_df['type'].values[0]}")
+    points_df.to_sql('points', conn, if_exists='append', index=False)
+    return points_df
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-    if after.channel is not None:
-        if after.channel.name == 'General':
-            utc_now = datetime.now(pytz.utc)
-            pacific_tz = pytz.timezone('US/Pacific')
-            pacific_time = utc_now.astimezone(pacific_tz)
-            join_time = pacific_time.strftime("%Y-%m-%d %H:%M:%S")
-            c.execute("INSERT INTO voice_log VALUES (?, ?, ?, ?)", (member.name, member.id, join_time, after.channel.name))
-            conn.commit()
-            print(f'Logged user {member.name} at {join_time}...')
-            print(pd.read_sql_query("SELECT * FROM voice_log", conn))
-            upload()
+    
+    # Get current time in Pacific timezone
+    current_time = _get_current_time()
+    pacific_time = datetime.strptime(current_time, "%Y-%m-%d %H:%M:%S")
+
+
+    # Check if the time is between 7am and 9am in Pacific timezone
+    if pacific_time.hour >= start_hour and pacific_time.hour < end_hour:
+        if after.channel is not None and after.channel.name == voice_channel:
+            # Get current time in UTC
+            join_time = _get_current_time()
+            append_to_database(member, after, join_time, joined=True)
             
+            # Send a message in the general text channel
+            await log_and_upload(member, join_time, True)
+
+        if before.channel is not None and before.channel.name == voice_channel:
+            leave_time = _get_current_time()
+            
+            append_to_database(member, before, leave_time, joined=False)
+            await add_points(current_text_channel(member))
+            await log_and_upload(member, leave_time, False)
+
+async def log_and_upload(member, event_time, joining):
+    logging_channel = current_text_channel(member)
+    if verbose:
+        await log_data(logging_channel, member, event_time, joining)
+    await upload(logging_channel)
+
+async def log_data(channel, member, event_time, joining):
+    leaving_str = "leaving " if not joining else ""
+    # await channel.send(f'Logged user {member.name} {leaving_str}at {event_time}...')
+    print(f'Logged user {member.name} {leaving_str}at {event_time}...')
+    print(pd.read_sql_query("SELECT * FROM voice_log", conn).tail())
+
+def append_to_database(member, event, event_time, joined):
+    c.execute("INSERT INTO voice_log VALUES (?, ?, ?, ?, ?)", (member.name, member.id, event_time, event.channel.name, joined))
+    conn.commit()
+
+def _get_current_time():
+    utc_now = datetime.now(pytz.utc)
+        
+    # Convert to Pacific time
+    pacific_tz = pytz.timezone('US/Pacific')
+    pacific_time = utc_now.astimezone(pacific_tz)
+        
+    # Format the time
+    join_time = pacific_time.strftime("%Y-%m-%d %H:%M:%S")
+    return join_time
+
+bot.run(bot_token)
+
+# # either 5:55am pacific or 6:55am pacific in UTC standard time, depending on daylight savings time
+# schedule.every().day.at("13:55").do(download)
+# schedule.every().day.at("14:55").do(download)
+
+
+# schedule.every().day.at("00:11").do(download)
+
+# while True:
+#     schedule.run_pending()
+    # time.sleep(1)
+
+# # Close the connection to the database
+
+# bot.run(bot_token)
+
+# # Close the connection to the database
+# conn.close()

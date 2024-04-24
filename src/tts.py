51a453cf496983f204d89f190834a6f2fb04d543
JordanLevy99 <jordanlevy99@gmail.com>
2024-04-24 13:39:25 -0700

    change start time for exercise_of_the_day to give 10 seconds buffer for calls to openai api

--- src/tts.py
+++ src/tts.py
@@ -155,7 +155,7 @@ class TTSTasks(commands.Cog):
         now = datetime.datetime.now()
         now = now.astimezone(pytz.timezone('US/Pacific'))
         target_time = datetime.datetime.replace(now, hour=self.bot.walk_constants.WINNER_HOUR,
-                                                minute=45, second=0,
+                                                minute=44, second=50,
                                                 microsecond=0)
         if now > target_time:
             target_time += datetime.timedelta(days=1)

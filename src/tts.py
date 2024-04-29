c901fb4c7919934cc933c2d91efbb36c6bb560fc
JordanLevy99 <jordanlevy99@gmail.com>
2024-04-28 21:05:43 -0700

    fix to getting the previous exercises, we now get the last (up to) 5 exercises for each difficulty

--- src/tts.py
+++ src/tts.py
@@ -23,15 +23,16 @@ class ExerciseOfTheDayResponseParser:
     - Duration
     - Points
     """
+
     def __init__(self, response):
         self.response = response
         self.exercise_map = {
-        'exercise': '',
-        'sets': '',
-        'reps': '',
-        'duration': '',
-        'difficulty': '',
-        'points': ''
+            'exercise': '',
+            'sets': '',
+            'reps': '',
+            'duration': '',
+            'difficulty': '',
+            'points': ''
         }
 
     def parse(self):
@@ -46,25 +47,29 @@ class ExerciseOfTheDayResponseParser:
 
 
 class TTSTasks(commands.Cog):
+    FULL_RESPONSE_SYSTEM_MESSAGE = (
+        "You are an assistant that is designed to motivate people who are on their morning walk"
+        " to do the exercise of the day. Announce the exercise, reps and/or duration, and the "
+        "number of points awarded from the user message in a motivational, "
+        "competitive, concise manner with some flair.")
 
-    FULL_RESPONSE_SYSTEM_MESSAGE = ("You are an assistant that is designed to motivate people who are on their morning walk"
-                              " to do the exercise of the day. Announce the exercise, reps and/or duration, and the "
-                              "number of points awarded from the user message in a motivational, "
-                              "competitive, concise manner with some flair.")
+    FORMATTED_RESPONSE_SYSTEM_MESSAGE = ('Given a difficulty ranging from easy to extreme '
+                                         '(with medium and hard inbetween), points awarded, '
+                                         'and recent previous exercises from that difficulty,'
+                                         ' you will create a unique exercise '
+                                         'that can be completed in one\'s home/apartment in 5-20 minutes '
+                                         '(depending on the difficulty), is different from the previous '
+                                         'exercises, and formatted in the following way with no extra text:'
+                                         '"Exercise: **{name_of_exercise}**\n'
+                                         '\tSets: **{number_of_sets}**\n'
+                                         '\tReps: **{number_of_reps}**\n'
+                                         '\tDuration: **{duration_per_set_or_total_duration_in_minutes_or_seconds}**\n'
+                                         '\tDifficulty: **{difficulty}**\n'
+                                         '\tPoints: **{points_awarded}**" '
+                                         'If any of this information is not needed for this exercise, output N/A for that field.')
 
-    FORMATTED_RESPONSE_SYSTEM_MESSAGE  = ('Given a difficulty ranging from easy to extreme '
-                                          '(with medium and hard inbetween), points awarded, '
-                                          'and the previous day\'s exercise you will create a unique exercise '
-                                          'that can be completed in one\'s home/apartment in 5-20 minutes '
-                                          '(depending on the difficulty), is different from the previous day\'s '
-                                          'exercise, and formatted in the following way with no extra text:'
-                                        '"Exercise: **{name_of_exercise}**\n'
-                                        '\tSets: **{number_of_sets}**\n'
-                                        '\tReps: **{number_of_reps}**\n'
-                                        '\tDuration: **{duration_per_set_or_total_duration_in_minutes_or_seconds}**\n'
-                                        '\tDifficulty: **{difficulty}**\n'
-                                        '\tPoints: **{points_awarded}**" '
-                                        'If any of this information is not needed for this exercise, output N/A for that field.')
+    NUM_EXERCISES = 5
+    DIFFICULTY_PROBABILITIES = [0.3, 0.5, 0.18, 0.02]
 
     def __init__(self, bot):
         self.bot = bot
@@ -81,10 +86,11 @@ class TTSTasks(commands.Cog):
             'Hard': 50,
             'Extreme': 100
         }
-        previous_exercise = self.__get_previous_exercise()
-        difficulty = np.random.choice(list(difficulty_points_map.keys()), p=[0.3, 0.5, 0.18, 0.02])
+        difficulty = np.random.choice(list(difficulty_points_map.keys()),
+                                      p=self.DIFFICULTY_PROBABILITIES)
+        previous_exercises = self.__get_previous_exercises(difficulty)
         points = difficulty_points_map[difficulty]
-        user_message = f"Previous day's exercise: {previous_exercise}\nDifficulty: {difficulty}\nPoints Awarded: {points}"
+        user_message = f"Previous exercises in this difficulty: {previous_exercises}\nDifficulty: {difficulty}\nPoints Awarded: {points}"
         print(user_message)
         tldr_response = self.create_chat(user_message,
                                          system_message=self.FORMATTED_RESPONSE_SYSTEM_MESSAGE)
@@ -121,6 +127,10 @@ class TTSTasks(commands.Cog):
                              duration=get_duration(local_speech_file_path),
                              start_second=0, download=False)
             await text_channel.send('\n\n' + tldr_response)
+        else:
+            text_channel = self.bot.discord_client.get_channel(self.bot.bot_constants.TEXT_CHANNEL_ID)
+            await text_channel.send(full_response)
+            await text_channel.send('\n\n' + tldr_response)
 
 
         response_parser = ExerciseOfTheDayResponseParser(tldr_response)
@@ -138,16 +148,20 @@ class TTSTasks(commands.Cog):
         upload(self.bot.backend_client, self.bot.bot_constants.DB_FILE)
         # await ctx.send(response.choices[0].text)
 
-    def __get_previous_exercise(self):
-        yesterday = datetime.datetime.now(tz=pytz.timezone('US/Pacific')).date() - datetime.timedelta(days=1)
+    def __get_previous_exercises(self, difficulty):
+        # yesterday = datetime.datetime.now(tz=pytz.timezone('US/Pacific')).date() - datetime.timedelta(days=1)
         try:
-            previous_exercise = self.bot.database.cursor.execute(f"""SELECT exercise FROM exercise_of_the_day 
-                                                                 WHERE date = {yesterday}""").fetchone()[0]
+            previous_exercises = list(self.bot.database.cursor.execute(f"""
+                            SELECT exercise FROM exercise_of_the_day 
+                            WHERE difficulty = '{difficulty}'
+                            ORDER BY date DESC 
+                            LIMIT {self.NUM_EXERCISES}""").fetchall())
+            previous_exercises = [exercise[0] for exercise in previous_exercises]
         except TypeError:
-            previous_exercise = None
-        if previous_exercise is None:
-            previous_exercise = 'N/A'
-        return previous_exercise
+            previous_exercises = None
+        if previous_exercises is None:
+            previous_exercises = ['N/A']
+        return ', '.join(previous_exercises)
 
     @exercise_of_the_day.before_loop
     async def before_exercise_of_the_day(self):
@@ -173,14 +187,14 @@ class TTSTasks(commands.Cog):
         current_date = current_time.date()
         current_time = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
         daily_exercise = self.bot.database.cursor.execute(f"SELECT exercise FROM exercise_of_the_day "
-                                                           f"WHERE date = "
-                                                           f"'{current_date}'").fetchone()[0]
+                                                          f"WHERE date = "
+                                                          f"'{current_date}'").fetchone()[0]
         exercise_points = self.bot.database.cursor.execute(f"SELECT points FROM exercise_of_the_day "
                                                            f"WHERE date = "
                                                            f"'{current_date}'").fetchone()[0]
         self.bot.database.cursor.execute(f"INSERT INTO exercise_log (name, id, exercise, time)"
                                          f" VALUES (?, ?, ?, ?)", (ctx.author.name, ctx.author.id,
-                                         daily_exercise, current_time))
+                                                                   daily_exercise, current_time))
         try:
             self.bot.database.cursor.execute(f"""INSERT INTO points (name, id, points_awarded, day, type)
                                                 VALUES (?, ?, ?, ?, ?)""",
@@ -197,7 +211,7 @@ class TTSTasks(commands.Cog):
     def __exercise_is_already_logged(self, ctx):
         current_time = datetime.datetime.now(tz=pytz.timezone('US/Pacific'))
         current_date = current_time.date()
-        return self.bot.database.cursor.execute(f"SELECT  name, id, exercise, date FROM " 
+        return self.bot.database.cursor.execute(f"SELECT  name, id, exercise, date FROM "
                                                 f"(SELECT name, id, exercise, DATE(time) as date "
                                                 f"FROM exercise_log)"
                                                 f"WHERE date = "
@@ -225,7 +239,7 @@ class TTSTasks(commands.Cog):
                 },
             ],
             model="gpt-4-0125-preview",
-            temperature=0.6
+            temperature=0.65
         )
 
         return response.choices[0].message.content

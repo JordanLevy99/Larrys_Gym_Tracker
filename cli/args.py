305659388b8dff5623267a350caae812d9cb9a80
Jordan Levy <jordanlevy99@gmail.com>
2025-06-03 15:20:43 -0400

    Add mute detection and logging

--- cli/args.py
+++ cli/args.py
@@ -1,7 +1,13 @@
 def parse_args():
     import argparse
+    import os
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--test', action='store_true', help='Run the bot in test mode')
     parser.add_argument('--verbose', action='store_true', help='Run the bot in verbose mode')
     parser.add_argument('--local', action='store_true', help='Run the bot in local mode')
-    return parser.parse_args()
+
+    args, _ = parser.parse_known_args()
+    if 'PYTEST_CURRENT_TEST' in os.environ:
+        args.local = True
+    return args

0d85c227166bf9afb41b88eaeb571f48b9c913d5
Jordan Levy <jordanlevy99@gmail.com>
2025-06-03 15:15:35 -0400

    Fix stock price update

--- src/extensions/stock_trading/larrys_stock_trader.py
+++ src/extensions/stock_trading/larrys_stock_trader.py
@@ -54,10 +54,11 @@ class Portfolio:
 
     def _update_stock_prices(self):
         for stock in self.stocks:
-            user_id, symbol, price = stock[0], stock[1], stock[-1]
+            user_id, symbol = stock[0], stock[1]
             # stock[-1] is price
-            stock[-1] = self.stock_api.get_current_price(symbol)
-            self.db.update_stock_price(user_id, symbol, price)
+            new_price = self.stock_api.get_current_price(symbol)
+            stock[-1] = new_price
+            self.db.update_stock_price(user_id, symbol, new_price)
         self.db.connection.commit()
 
 
